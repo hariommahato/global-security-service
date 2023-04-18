3a2ede98967cd4ccc1baef00bbbb818a33708748
hariommahato <71360153+hariommahato@users.noreply.github.com>
2023-04-18 10:42:08 +0545

    Delete pages/api directory

--- pages/api/register.js
+++
@@ -1,8 +0,0 @@
-import dbConnect from "@/backend/config/dbConnect";
-import { registerUser } from "../../backend/controllers/userController";
-import nextConnect from "next-connect";
-
-const handler = nextConnect();
-dbConnect();
-handler.post(registerUser);
-export default handler;
